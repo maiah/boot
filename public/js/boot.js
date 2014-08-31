5ebb839ed98350e91f4b8e54cc83d42c7c7369ae
maiah <jjmmacariola@gmail.com>
2014-08-31 23:35:09 +0800

    Added client side co request to server example

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,7 +1,12 @@
-console.log('Hello world');
+require('setimmediate');
 
-if (true) {
-    console.log('cool here...');
-} else {
-    console.log('This is not so cool.');
-}
+var co = require('co');
+var thunkify = require('thunkify');
+var request = require('request');
+var get = thunkify(request.get);
+
+co(function* () {
+    var res = yield get('http://localhost:7000/js/boot.js');
+    console.log('Status is ', res[0].statusCode);
+    console.log('Content is\n', res[1]);
+})();
